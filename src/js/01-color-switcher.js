7f983b4288f1c05a7edc8dbe5f5524068ec18c1c
NickKononenko <kononenko0812@gmail.com>
2023-08-15 13:12:58 +0300

    1st task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+let interval;
+
+refs.start.addEventListener('click', onStart);
+refs.stop.addEventListener('click', onStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStart() {
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.disabled = true;
+}
+
+function onStop() {
+  clearInterval(interval);
+  refs.start.disabled = false;
+}
