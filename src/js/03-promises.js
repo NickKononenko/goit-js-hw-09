4687146ab78b056808d80f8537eafaa875391b69
NickKononenko <kononenko0812@gmail.com>
2023-08-19 21:13:20 +0300

    some bugs fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,7 @@
+const form = document.querySelector('.form');
+
+console.dir(form);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
